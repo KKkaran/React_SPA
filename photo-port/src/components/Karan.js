f000ffee117a573ef6b40363492d829cddead7cd
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-02-27 13:38:15 -0500

    first component made

---
+++ photo-port/src/components/Karan.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Karan = ()=>{
+    return (
+        <div>
+            <h2>Karan Sodhi</h2>
+            <p>This is the para from me</p>
+        </div>
+    )
+}
+
+
+export default Karan
